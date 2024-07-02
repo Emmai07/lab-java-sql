5183b0b03769083127657b3a8ff9bbb5841190b7
Emma <emma.saavedra002@mymdc.net>
2024-07-02 18:55:12 -0400

    removed the main from airline queries
    removed the main from airline queries

--- Data Layer SQL/src/main/java/com/example/AirlineQueries.java
+++ Data Layer SQL/src/main/java/com/example/AirlineQueries.java
@@ -138,19 +138,5 @@ public class AirlineQueries {
         }
     }
 
-    public static void main(String[] args) {
-        try {
-            System.out.println("Total number of flights: " + getTotalNumberOfFlights());
-            System.out.println("Average flight distance: " + getAverageFlightDistance());
-            System.out.println("Average number of seats: " + getAverageNumberOfSeats());
-            getAverageMilesFlownByCustomerStatus();
-            getMaxMilesFlownByCustomerStatus();
-            System.out.println("Total number of Boeing aircraft: " + getTotalNumberOfBoeingAircraft());
-            getFlightsBetweenDistance(300, 2000);
-            getAverageFlightDistanceByCustomerStatus();
-            getMostOftenBookedAircraftByGoldMembers();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+  
 }
